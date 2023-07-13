18ea548813632fa52160d99c21267af956f67bb4
Tanja Gerke <tanja.kupfer@gmx.de>
2023-07-14 00:44:10 +0200

    todo Liste erstellen

--- udemy kurs Javascript/15 - Projekt Todo-Liste/Teil 00 (Ausgangsbasis)/js/app.js
+++ udemy kurs Javascript/15 - Projekt Todo-Liste/Teil 00 (Ausgangsbasis)/js/app.js
@@ -1,5 +1,38 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
+const KEY_ENTER = 13
 
+document.addEventListener("DOMContentLoaded", () => {
+    const newTodoElement = document.querySelector(".new-todo")
+    const todoListElement = document.querySelector(".todo-list")
+
+
+    newTodoElement.addEventListener("keypress", (event) => {
+        if (event.witch === KEY_ENTER && newTodoElement.value !== "") {
+            const newButtonElement = document.createElement("button")
+            newButtonElement.classList.add("destroy")
+
+            const newLabelElement = document.createElement("label")
+            newLabelElement.appendChild(
+                document.createTextNode(newTodoElement.value)
+            )
+            const newInputCheckbox = document.createElement("input")
+            newInputCheckbox.type = "checkbox"
+            newInputCheckbox.classList.add("toggle")
+
+            const newDivElement = document.createElement("div")
+            newDivElement.classList.add("view")
+            newDivElement.appendChild(newInputCheckbox)
+            newDivElement.appendChild(newLabelElement)
+            newDivElement.appendChild(newButtonElement)
+
+            const newLiElement = document.createElement("li")
+            newLiElement.appendChild(newDivElement)
+
+            todoListElement.prepend(newLiElement)
+
+            newTodoElement.value = ""
+
+        }
+    })
 });
